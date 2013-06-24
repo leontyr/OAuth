0468d1df96deae6ea5b572e42105cf67b445adaa
Yiran Tian <leontyr@gmail.com>
2013-06-24 02:24:18 +0100

    new

---
+++ main.js
@@ -0,0 +1,22 @@
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/trunk/apps/app.runtime.html
+ * @see http://developer.chrome.com/trunk/apps/app.window.html
+ */
+chrome.app.runtime.onLaunched.addListener(function() {
+  // Center window on screen.
+  var screenWidth = screen.availWidth;
+  var screenHeight = screen.availHeight;
+  var width = 1000;
+  var height = 800;
+
+  chrome.app.window.create('index.html', {
+    bounds: {
+      width: width,
+      height: height,
+      left: Math.round((screenWidth-width)/2),
+      top: Math.round((screenHeight-height)/2)
+    }
+  });
+});

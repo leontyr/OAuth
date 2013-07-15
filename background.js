dc1314fec17c4cd2d1a953c53e6513b06e516b7e
Leon <leontyr@yahoo.com>
2013-07-15 14:18:02 +0100

    app

--- background.js
+++ background.js
@@ -4,21 +4,21 @@
  * @see http://developer.chrome.com/trunk/apps/app.runtime.html
  * @see http://developer.chrome.com/trunk/apps/app.window.html
  */
-window.open('index.html');
+//window.open('index.html');
 
-//chrome.app.runtime.onLaunched.addListener(function () {
-//   //Center window on screen.
-//  var screenWidth = screen.availWidth;
-//  var screenHeight = screen.availHeight;
-//  var width = 1000;
-//  var height = 800;
+chrome.app.runtime.onLaunched.addListener(function () {
+   //Center window on screen.
+  var screenWidth = screen.availWidth;
+  var screenHeight = screen.availHeight;
+  var width = screenWidth/2;
+  var height = screenHeight/2;
 
-//  chrome.app.window.create('index.html', {
-//    bounds: {
-//      width: width,
-//      height: height,
-//      left: Math.round((screenWidth-width)/2),
-//      top: Math.round((screenHeight-height)/2)
-//    }
-//  });
-//});
+  chrome.app.window.create('index.html', {
+    bounds: {
+      width: width,
+      height: height,
+      left: Math.round((screenWidth-width)/2),
+      top: Math.round((screenHeight-height)/2)
+    }
+  });
+});
